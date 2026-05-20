1e05a7cd616fb0069c369772c5f8279960ea6e32
sumitasinha13-rgb <sumitasinha13@gmail.com>
2026-05-20 17:26:08 +0530

    Add javascript dashboard page

---
+++ app/page.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div style={{padding:"40px",fontFamily:"Arial"}}>
+      <h1>📈 AI NSE Smart Opportunity Finder</h1>
+      <h2>Dashboard Working Successfully 🚀</h2>
+
+      <div>
+        <p>TCS | Score: 88 | Risk: Medium</p>
+        <p>Reliance | Score: 85 | Risk: Medium</p>
+        <p>HDFC Bank | Score: 84 | Risk: Low</p>
+      </div>
+    </div>
+  );
+}
