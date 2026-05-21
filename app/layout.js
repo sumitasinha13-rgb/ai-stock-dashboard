58314be89dc6fcda0ccd0420bc5783d404104a7d
sumitasinha13-rgb <sumitasinha13@gmail.com>
2026-05-21 17:30:57 +0530

    Add app layout

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "AI Stock Dashboard",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+}
